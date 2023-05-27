fc878a25e84bf5c97c6093d45334231ad89db37f
Dorhan2 <andru20132@mail.ru>
2023-05-27 19:29:21 +0500

    Start Sokoban'ы есть движение(не многопоток)

--- robots/src/gui/Sokobana.java
+++ robots/src/gui/Sokobana.java
@@ -7,10 +7,15 @@ import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.awt.Dimension;
 
+import java.util.HashMap;
+import java.util.Vector;
+
+
+
 @SuppressWarnings("serial")
 public class Sokobana extends JPanel {
-    public static final int PANEL_WIDTH = 400;
-    public static final int PANEL_HEIGHT = 375;
+    public static final int PANEL_WIDTH = 380;
+    public static final int PANEL_HEIGHT = 355;
     public static final int SQUARE_SIZE = 20;
     public static final int SPEED = 10;
     public static final Color SQUARE_COLOR = Color.RED;
@@ -65,6 +70,33 @@ public class Sokobana extends JPanel {
         super.paintComponent(g);
         g.setColor(SQUARE_COLOR);
         g.fillRect(xPos, yPos, SQUARE_SIZE, SQUARE_SIZE);
-        g.fillRect(100, 100, SQUARE_SIZE, SQUARE_SIZE);
+        g.setColor(Color.ORANGE);
+        g.fillRect(PANEL_WIDTH, 0, 10, PANEL_HEIGHT);
+        g.fillRect(0, PANEL_HEIGHT, PANEL_WIDTH + 10, 10);
     }
+//    public boolean AABBvsAABB(AABB a, AABB b ){
+//        // Выходим без пересечения, потому что найдена разделяющая ось
+//        if (a.max.x < b.min.x or a.min.x > b.max.x) return false;
+//        if(a.max.y < b.min.y or a.min.y > b.max.y) return false;
+//
+//        // Разделяющая ось не найдена, поэтому существует по крайней мере одна пересекающая ось
+//        return true
+//    }
 }
+
+
+
+
+
+//    class AABB {
+//        Vector min;
+//        Vector max;
+//        };
+//
+//
+//    class Manifold{
+//        Object A;
+//        Object B;
+//        float penetration;
+//        Vector normal;
+//        };
